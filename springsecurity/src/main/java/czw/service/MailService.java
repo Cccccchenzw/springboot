ad994f657d7d0ed51a0fb8ea3d4c02cfd864f5c3
unknown <180878@internet.com>
2022-04-20 16:15:33 +0800

    邮件+简单打包0422

--- springsecurity/src/main/java/czw/service/MailService.java
+++ springsecurity/src/main/java/czw/service/MailService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MailService {
-    @Autowired
+    @Autowired(required = false)
     JavaMailSender javaMailSender;
 
     public void sendSimpleMail(String from,String to,String subject,String content){
